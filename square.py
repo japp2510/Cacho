d8f4434cb97428744814fefcb6e49748a9df0b5f
Juan Ariel Pommier <juanariel1991@gmail.com>
2021-03-19 18:33:05 +0100

    Class Poker and Big were added

--- square.py
+++ square.py
@@ -74,7 +74,6 @@ class Full:
                 else:
                     break
             condition = self.is_full()
-            print(condition)
             self.score = (30 + (5 if first else 0)) if condition else 0
             self.selected = True
 
@@ -90,14 +89,84 @@ class Full:
             return "$"
 
 
-dice = [Die(2), Die(1), Die(3), Die(3), Die(3)]
+class Poker:
+    def __init__(self):
+        self.score = 0
+        self.selected = False
+        self.x = {'face': 0, 'cant': 0}
+        self.y = {'face': 0, 'cant': 0}
 
-full = Full()
+    def is_poker(self):
+        return ((self.x['cant'] + self.y['cant']) == 5) & (self.x['cant'] == 4 or self.y['cant'] == 4)
+
+    def calculate_score(self, dice, first=False):
+        if not self.selected:
+            for die in dice:
+                if self.x['face'] == 0 or self.x['face'] == die.get_face():
+                    self.x['face'] = die.get_face()
+                    self.x['cant'] += 1
+                elif self.y['face'] == 0 or self.y['face'] == die.get_face():
+                    self.y['face'] = die.get_face()
+                    self.y['cant'] += 1
+                else:
+                    break
+            condition = self.is_poker()
+            self.score = (40 + (5 if first else 0)) if condition else 0
+            self.selected = True
+
+    def get_score(self):
+        return self.score
+
+    def __repr__(self):
+        if self.score == 0:
+            return str(0)
+        elif self.score == 40:
+            return "()"
+        elif self.score == 45:
+            return "$"
+
+
+class Big:
+    def __init__(self):
+        self.score = 0
+        self.selected = False
+        self.x = {'face': 0, 'cant': 0}
+
+    def is_big(self):
+        return self.x['cant'] == 5
+
+    def calculate_score(self, dice, first=False):
+        if not self.selected:
+            for die in dice:
+                if self.x['face'] == 0 or self.x['face'] == die.get_face():
+                    self.x['face'] = die.get_face()
+                    self.x['cant'] += 1
+                else:
+                    break
+            condition = self.is_big()
+            self.score = (50 + (50 if first else 0)) if condition else 0
+            self.selected = True
+
+    def get_score(self):
+        return self.score
+
+    def __repr__(self):
+        if self.score == 0:
+            return str(0)
+        elif self.score == 50:
+            return "$"
+        elif self.score == 100:
+            return "$$"
+
+
+dice = [Die(3), Die(3), Die(3), Die(3), Die(3)]
+
+big = Big()
 for die in dice:
     die.throw()
     print("---")
     print(die)
     print("\n")
 
-full.calculate_score(dice)
-print(full)
+big.calculate_score(dice)
+print(big)
